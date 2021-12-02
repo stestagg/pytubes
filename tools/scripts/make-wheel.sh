003b2a5045417317667559ce39ddd0a005208e70
Steve Stagg <stestagg@gmail.com>
2021-12-02 16:43:21 +0000

    Bump auditwheel requirement

--- tools/scripts/make-wheel.sh
+++ tools/scripts/make-wheel.sh
@@ -16,7 +16,7 @@ pip wheel . -w wheelhouse/
 
 WHEEL=wheelhouse/pytubes-*-$1-linux_x86_64.whl
 auditwheel show $WHEEL
-auditwheel repair --plat manylinux2010_x86_64 -w wheelhouse/ $WHEEL
+auditwheel repair --plat manylinux2014_x86_64 -w wheelhouse/ $WHEEL
 rm wheelhouse/pytubes-*-$1-linux_x86_64.whl
 
 if [ ! -z x"$GITHUB_WORKSPACE" ]; then
